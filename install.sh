9be46a613a7f7142267f68316d8f81bda6b87e56
jakebudin <jacob.budin@mymail.champlain.edu>
2013-03-09 23:39:23 -0500

    Update install.sh
    ***Specify  the OS information
    
    Added the proper commands to install and build Boot2Gecko Firefox OS.
    Use this command to run the emulator: ./run-emulator.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-echo "I don't know what I am doing!!!"
+sudo apt-get install autoconf2.13 bison bzip2 ccache curl flex gawk gcc g++ g++-multilib git ia32-libs lib32ncurses5-dev lib32z1-dev libgl1-mesa-dev libx11-dev make zip
+
+sudo apt-get upgrade
+
+sudo apt-get update
+
+echo "I barely know what I am doing"
 
 # What Linux distro are we on?
 OS= sed -n 's/^NAME=//p' /etc/*-release
@@ -8,6 +14,17 @@ echo $OS
 
 sudo apt-get install gcc -y
 # yum install gcc -y
+
+ccache --max-size 3GB
+
+git clone git://github.com/mozilla-b2g/B2G.git
+cd B2G/
+./config.sh emulator
+
+git pull
+./repo sync
+
+./build.sh
 
 exit 0
 
