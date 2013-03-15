26eda32389bb4a5852c509d8e89063948109c4c1
jakebudin <jacob.budin@mymail.champlain.edu>
2013-03-15 19:48:36 -0300

    Update install.sh
    Linux distro spec

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 echo "I barely know what I am doing"
 
 # What Linux distro are we on?
-OS= sed -n 's/^NAME=//p' /etc/*-release
+OS= sed -n 's/^UBUNTU=//p' /etc/*-release
 echo $OS
 
 sudo apt-get install gcc -y
